8cb6684c04472f6aef0edaf8e8bae586541a5827
taymoorgr <www.taymoor@outlook.com>
2024-12-20 04:31:21 +0500

    Removed footer content

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,6 +9,8 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}></p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}></p>
       </div>
     </Fade>
   );
